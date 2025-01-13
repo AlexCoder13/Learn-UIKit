dff13e52412ce809941e32d75dc9384c3db4cc76
AlexCoder13 <sesasha99@gmail.com>
2025-01-14 00:04:52 +0300

    Initial Commit

---
+++ T-2hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T-2hw
+//
+//  Created by Александр Семёнов on 14.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
